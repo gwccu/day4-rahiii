f8590560c62eb9f5a9c068302ba210da80501387
rahiii <rahimitranyc@gmail.com>
2019-03-02 16:19:07 +0000

    day 4 warm up

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,6 +1,6 @@
 # file name: problemSetDay4.py
 player = input('rock (r), paper (p), or sissors (s)?')
-print(player, 'vs', end=' ')
+print(player, 'vs')
 from random import randint
 chosen = randint(1,3)
 #print(chosen)
@@ -10,8 +10,21 @@ if chosen == 1:
 elif chosen == 2:
     computer = 'p'
 else:
-    computer - 's'
+    computer = 's'
 
 print(computer)
 if player == computer:
-    print('draw)')
+    print('draw')
+elif player == 'r' and computer == 's':
+    print('Player wins!')
+elif player == 's' and computer == 'r':
+    print('Computer win!')
+elif player == 'p' and computer == 'r':
+    print("Computer wins!")
+elif player == 'r' and computer == 'p':
+    print("Player wins!")
+elif player == 's' and computer == 'p':
+    print('Computer wins!')
+elif player == 'p' and computer == 's':
+    print('Player wins!')
+    
