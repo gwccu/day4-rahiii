4e59517f4ef8f91c96a2c204c29277c467e16e7b
rahiii <rahimitranyc@gmail.com>
2019-03-02 15:36:49 +0000

    warm up

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,1 +1,17 @@
 # file name: problemSetDay4.py
+player = input('rock (r), paper (p), or sissors (s)?')
+print(player, 'vs', end=' ')
+from random import randint
+chosen = randint(1,3)
+#print(chosen)
+
+if chosen == 1:
+    computer = 'r'
+elif chosen == 2:
+    computer = 'p'
+else:
+    computer - 's'
+
+print(computer)
+if player == computer:
+    print('draw)')
